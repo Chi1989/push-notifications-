c5731168f170a127f4254d6978490cdc36b4d468
Chi1989 <agbachichidiogomelvis@gmail.com>
2022-12-08 04:37:02 +0100

    added

--- push notifications/push notifications/ViewController.swift
+++ push notifications/push notifications/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
     
     func registerLocalNotification() {
         
+        //MARK: - User permission
         UNUserNotificationCenter.current().delegate = self
         let centre = UNUserNotificationCenter.current()
         centre.requestAuthorization(options: [.badge, .alert
@@ -27,29 +28,28 @@ class ViewController: UIViewController, UNUserNotificationCenterDelegate {
                 print("user is granted \(granted)")
             }
         }
+        
+        //MARK: - Create notification content
         let content = UNMutableNotificationContent()
         content.title = "Hello Chi"
         content.body = "Kindly see the attached offer letter of employment, acknowledge and sign"
         content.sound = .default
         content.userInfo = ["value": "Data with local notification"]
         
-        
+        //MARK: - The notification trigger
         let date = Date().addingTimeInterval(20)
         let dateComponent = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
-        
-        
         let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: false)
         
-        
+        //MARK: - The notification request
         let request = UNNotificationRequest(identifier: "reminder", content: content, trigger: trigger)
         
-        
+        //MARK: - Register the request
         centre.add(request) { (error) in
             if error != nil {
                 print("Error = \(error?.localizedDescription ?? "error local notification")")
             }
         }
-        
         
     }
 
